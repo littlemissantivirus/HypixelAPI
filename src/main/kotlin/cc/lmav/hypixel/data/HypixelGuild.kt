edc0e87d0b4d9e31e33e3b22a11363602504af5a
littlemissantivirus <littlemissantivirus@gmail.com>
2020-05-05 22:40:18 -0300

    Start to fix some formatting.

--- src/main/kotlin/cc/lmav/hypixel/data/HypixelGuild.kt
+++ src/main/kotlin/cc/lmav/hypixel/data/HypixelGuild.kt
@@ -1,14 +1,47 @@
 package cc.lmav.hypixel.data
 
 data class HypixelGuildWrapper(val success: Boolean, val guild: HypixelGuild)
-data class HypixelGuild(val _id: String, val name: String, val coins: Int, val coinsEver: Int, val created: Long,
-                        val members: List<HypixelGuildMember>, val banner: HypixelGuildBanner, val tag: String,
-                        val legacyRanking: Int, val exp: Long, val achievements: Map<String, Int>, val ranks: List<HypixelGuildRank>,
-                        val publiclyListed: Boolean, val preferredGames: List<String>, val name_lower: String, val tagColor: String,
-                        val hideGmTag: Boolean, val guildExpByGameType: Map<String, Int>)
 
-data class HypixelGuildMember(val uuid: String, val rank: String, val joined: Long, val questParticipation: Int,
-                              val expHistory: Map<String, Int>)
+data class HypixelGuild(
+        val _id: String,
+        val achievements: Map<String, Int>,
+        val banner: HypixelGuildBanner,
+        val chatMute: Long,
+        val coins: Int,
+        val coinsEver: Int,
+        val created: Long,
+        val description: String,
+        val exp: Int,
+        val expByGameType: Map<String, Int>,
+        val hideGmTag: Boolean,
+        val joinable: Boolean,
+        val legacyRanking: Int,
+        val members: List<HypixelGuildMember>,
+        val name: String,
+        val name_lower: String,
+        val preferredGames: List<String>,
+        val publiclyListed: Boolean,
+        val ranks: List<HypixelGuildRank>,
+        val tag: String,
+        val tagColor: String
+)
+
+data class HypixelGuildMember(
+        val uuid: String,
+        val rank: String,
+        val joined: Long,
+        val questParticipation: Int,
+        val expHistory: Map<String, Int>
+)
+
 data class HypixelGuildBanner(val Base: Int, val Patterns: List<HypixelGuildPattern>)
+
 data class HypixelGuildPattern(val Pattern: String, val Color: Int)
-data class HypixelGuildRank(val name: String, val default: Boolean, val tag: String, val created: Long, val priority: Int)
+
+data class HypixelGuildRank(
+        val name: String,
+        val default: Boolean,
+        val tag: String,
+        val created: Long,
+        val priority: Int
+)
